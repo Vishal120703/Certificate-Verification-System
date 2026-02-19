ce64fdd9057fa63f0bf982675035ac04464d526d
vishal gupta <vishalgupta120703@gmail.com>
2026-02-19 10:29:56 +0530

    now, this commit is for basic setup of frontend is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/Login';
-import ProtectedRoute from './components/ProtectedRoute';
-import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
-    <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>} />
-    </Routes>
-    </BrowserRouter>
-    </>
+    <div>App</div>
   )
 }
 
